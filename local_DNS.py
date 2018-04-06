f86ebd3cb9955519bbe0377417cb6e6e49b74d4c
c54wang <c54wang@scs.ryerson.ca>
2018-04-06 15:46:25 -0400

    local_DNS

---
+++ local_DNS.py
@@ -0,0 +1,15 @@
+import socket
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 40040
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+
+while True:
+    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    print "received message:", data
+
+    if data:
+        print addr
+        sock.sendto(b'This is the resolved IP address', addr)
--- website.py
+++ website.py
@@ -1,9 +1,38 @@
 from flask import Flask
+from flask import render_template
+import config
+import socket
+
 app = Flask(__name__)
 
+#The default URL to go to:
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return "Local DNS Port:"
+    return str(config.LOCAL_DNS_PORT)
+
+
+
+@app.route('/hello/')
+def hello():
+    return render_template('index.html')
+
+@app.route('/hello2/')
+def hello2():
+    UDP_IP = "127.0.0.1"
+    UDP_PORT = 40040
+    MESSAGE = "Hello, World!"
+
+    print "UDP target IP:", UDP_IP
+    print "UDP target port:", UDP_PORT
+    print "message:", MESSAGE
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+
+    data, addr = sock.recvfrom(1024)
+    return str(data)
+
 
 if __name__ == "__main__":
     app.run()
