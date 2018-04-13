51c2a25433fca72a560155f4087806650d296557
pkkamboj <pkamboj@ryerson.ca>
2018-04-13 01:06:17 -0400

    index nto displaying

--- client.py
+++ client.py
@@ -6,24 +6,35 @@
 # Step 1: client to do all the opening, gets everything, tcp conenction with hiscinema sends index file, client displays it 
 import socket
 import config
+from flask import Flask
+from flask import render_template
 
 # https://wiki.python.org/moin/TcpCommunication
-tcpip = config.hisCinema_Host
+tcpip = config.hisCinema_HOST
 tcpport = config.hisCinema_PORT
 buffersize = 1024
 message = "Send hiscinema.com html file with video links"
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(tcpip, tcpport);
+sock.connect((tcpip, tcpport));
 sock.send(message)
-print"Client requests videos.hiscinema.com from www.hiscinema.com web server at IP address: " + tcpip + " and port: " + tcpport
+print"Client requests videos.hiscinema.com from www.hiscinema.com web server at IP address: ",tcpip," and port: ",tcpport
 
 #hiscinema receives this request and sends over index.html
+data = sock.recv(buffersize)
+print "Receiving index.html..."
+sock.close()
 
-(name, value, type)
-(video.hiscinema.com, hiscinemaDNS, NS)
-(name, value, V - video)
-(name, value, R - redirect)
+#displays index.html file on browser
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return str(data)
+
+if __name__ == "__main__":
+    app.run()
+
 
 # Step 2: when video is clicked to be downloaded, request sent to localDNS for IP address of video.hiscinema.com/v1 which is in hiscinemaDNS
 # Step 3: localDNS then request for video.hiscinema.com IP from hiscinemaDNS, hisCinemaDNS responds with herCDNDNS ip address NS type
--- hisCinema.py
+++ hisCinema.py
@@ -1,3 +1,31 @@
 import config
 import socket 
 
+#tcp connection established with client to sen index file
+tcpip = config.hisCinema_HOST
+tcpport = config.hisCinema_PORT
+buffersize = 1024
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind((tcpip,tcpport))
+sock.listen(1)
+
+conn, addr = sock.accept()
+data = conn.recv(buffersize)
+print "Message received from address: ",addr,"saying: ",data
+
+while True:
+	print "Opening index.html..."
+	fil = open('templates/index.html', 'rb')
+
+	print "Reading index.html..."
+	line = fil.read(1024)
+	while (line):
+		conn.send(line)
+		line = fil.read(1024)
+
+	print "index.html sent."
+
+	fil.close()
+	conn.close()
+	print "Connection Closed"
